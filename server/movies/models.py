240b1803fb0a0e6058adf3d3b5858627bc8d9ac8
Song-d-s <s2770853@gmail.com>
2022-11-17 17:44:28 +0900

    migration failed

---
+++ server/movies/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField 
+
+# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=64)
+
+class Keyword(models.Model):
+    name = models.CharField(max_length=128)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=128)
+    release_date = models.CharField(max_length=128)
+    vote_average = models.FloatField()
+    overview = models.TextField()
+    backdrop_path = models.TextField()
+    paster_path = models.TextField()
+    genres = models.ManyToManyField(Genre, related_name='movie_genres')
+    keywords = models.ManyToManyField(Keyword, related_name='movie_keywords', blank=True)
+    director = models.CharField(max_length=512, blank=True)
+    cast = models.CharField(max_length=512, blank=True)
+    vote_average_naver = models.FloatField(blank=True)
+    link_naver = models.TextField()
+
+
+
+
--- server/server/settings.py
+++ server/server/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # local apps
-    'todos',
+    'movies',
     'accounts',
 
     # drf
