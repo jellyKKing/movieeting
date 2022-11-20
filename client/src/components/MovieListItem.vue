3c4f5ccd2f80715fcb250ac2d06515394c019b43
Dansaem Song <s2770853@gmail.com>
2022-11-20 23:37:42 +0900

    1120 minor updates - arrow button added to swipers - responsive layout on detail page - footer

--- client/src/components/MovieListItem.vue
+++ client/src/components/MovieListItem.vue
@@ -50,7 +50,8 @@ export default {
   border: 1px solid hsl(210,16.7%,97.6%, 10%);
   border-radius: 5px;
   transition: scale 0.3s;
-  box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+  /* box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px; */
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 20px 30px;
   /* max-height: 20rem; */
 }
 .poster-img-box:hover{
@@ -84,7 +85,7 @@ export default {
   margin: 0;
   padding: 0;
   font-size: 12px;
-  line-height: 8px;
+  line-height: 12px;
   opacity: 50%;
 }
 /* card css */
