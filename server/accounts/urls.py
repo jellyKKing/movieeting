7e3bf853abeb52cf617251e6f691c3481b24c404
Song-d-s <s2770853@gmail.com>
2022-11-25 08:34:48 +0900

    삭제

--- server/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.kakaoLoginView),
-    path('mypage/', views.mypage),
-    path('user/<int:user_id>/', views.userpage),
-]
